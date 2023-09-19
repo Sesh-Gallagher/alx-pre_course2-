09409fd208897facc7f9955f5c84d4362930dfd2
Sesh-Gallagher <t.ntwana@gmail.com>
2023-09-19 03:50:56 -0700

    loadshedding

--- Shell_test/main.c
+++ Shell_test/main.c
@@ -15,30 +15,62 @@ int _printstring(char *str)
 	return (count);
 }
 
-int main(void)
+int main(int c, char **env)
 {
 	char *prompt =  "(Test_Shell)$ ", *buffer = NULL;
+	(void)c;
 	pid_t Child_ID;
 	size_t buffersize = 0;
+	int status, i, j;
 	ssize_t num_chars;
+	char *arg[10], *delim = "\n";
 
 	while (1)
 	{
 
-	_printstring(prompt);
+	if (isatty(0))
+		_printstring(prompt);
 
 	num_chars = getline(&buffer, &buffersize, stdin);
 
 
 	if (num_chars == -1)
 	{
-		_printstring("Exiting shell ...");
-		return (-1);
+		free(buffer);
+		exit (0);
+	}
+
+	i = 0;
+	while (buffer[i])
+	{
+		if (buffer[i] == '\n')
+			buffer[i] = 0;
+		i++;
+	}
+
+	j = 0;
+	arg[j] = strtok(buffer, delim);
+	while (arg[j])
+	{
+		arg[++j] = strtok(NULL, delim);		
 	}
 
 	Child_ID = fork();
+	if (Child_ID < 0)
+	{
+		_printstring("Forking failed");
+		free(buffer);
+		exit (0);
+	}
+	else if (Child_ID == 0)
+	{
+	if (execve(arg[0], arg, env) == -1);
+		_printstring("Command not found\n");
+	}
+	else
+		wait(&status);
 
-	_printstring(buffer);
+	/*_printstring(buffer);*/
 
 	}
 	free(buffer);
